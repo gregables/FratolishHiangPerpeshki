a4a80176158b81bddeca537aa04ab18a22754af9
gregables <joebow_123@hotmail.co.uk>
2020-11-24 01:45:07 +0000

    Initial commit

---
+++ fratolishhiangperpeshki/src/main/scala/fratolishhiangperpeshki/FratolishhiangperpeshkiWorker.scala
@@ -0,0 +1,27 @@
+package fratolishhiangperpeshki
+
+import wncomwnco.thePrinter
+case class functions[-inputTP,+outputTP](sumparam:inputTP=>outputTP) {
+    def dowit(a:inputTP):outputTP=sumparam(a)
+  }
+  class animal
+  class doggo extends animal
+  class smalldoggo extends doggo
+  class cat extends animal
+  
+ object implementation {
+    def thisFunction(sumit:doggo):doggo=sumit
+    def thatFunction(a:smalldoggo):animal=a
+    def onemoretime(a:functions[smalldoggo,animal])=a.dowit(new smalldoggo)
+    val thingy = functions(thisFunction)
+    val thongo = functions(thatFunction)
+    val punchLine = onemoretime(thingy)
+    case class box[+ii](a:ii) {
+        def newBox[jj>:ii](funcy:jj):box[jj] = box(funcy)
+    }
+    val sm = box(new doggo)
+    val cast:box[animal] = sm
+    val tst = cast.newBox(new cat)
+    val eric = box(new smalldoggo).newBox(new doggo)
+    val robbo = thePrinter.point1
+}
---
+++ wncomwnco/src/main/scala/wncomwnco/WncomwncoWorker.scala
@@ -0,0 +1,16 @@
+package wncomwnco
+class Point(var x: Int, var y: Int) {
+
+  def move(dx: Int, dy: Int): Unit = {
+    x = x + dx
+    y = y + dy
+  }
+
+  override def toString: String =
+    s"($x, $y)"
+}
+object thePrinter{
+    val point1 = new Point(2, 3)
+    println(point1.x)  // 2
+    println(point1)  // prints (2, 3)
+}
